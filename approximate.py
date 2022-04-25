f87303b054a20f4f245d41142c382ac345006c0a
Luke Ogburn <21106956+logburn@users.noreply.github.com>
2022-04-25 16:17:51 -0400

    added concept for approximation solution based on ACO

---
+++ approximate.py
@@ -0,0 +1,19 @@
+
+def main():
+    pass
+    # concept: ant colony optimization (ACO)
+    #  FOR EACH ANT:
+    #   1. probabalisticaly select an edge based on the weight and pheromone levels
+    #   2. travel to that path, adding pheromones to the edge
+    #      - traveling down a path takes time, so an ant might be skipped in a round
+    #        due to it traveling to a node during that time
+    #   3. repeat until no more nodes are possible, and return home
+    # place ants randomly and allow them to follow this structure. decrease pheromones slightly
+    # for each round.
+    
+    # questions:
+    #  - where do ants start? randomly, or multiple ants at one starging node?
+    
+
+if __name__ == "__main__":
+    main()
