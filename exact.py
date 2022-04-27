32d201807134e5f60a8dc7cfc62aa1b529dcae9f
DEVEN SARMA <sarmada@dukes.jmu.edu>
2022-04-27 14:44:25 -0400

    permuations made and input improved, need to add the start node as input.

--- exact.py
+++ exact.py
@@ -5,6 +5,7 @@ def main():
     nodes = int(input())
     num_edges = int((nodes*(nodes-1))/2)
     adj_list = {}
+    visited = {}
     for _ in range(num_edges):
         u, v, w = [num for num in input().split()]
         w = int(w)
@@ -12,10 +13,22 @@ def main():
             adj_list[u].append((v, w))
         else:
             adj_list[u] = [(v, w)]
+        if v in adj_list:
+            adj_list[v].append((u, w))
+        else:
+            adj_list[v] = [(u, w)]
+        if u not in visited:
+            visited[u] = "unmarked"
+        if v not in visited:
+            visited[v] = "unmarked"
 
     print(adj_list)
+    print(visited)
 
-
+    p = itertools.permutations(adj_list)
+    # leave the start vertex out and place at the start and end
+    for j in list(p):
+        print(j)
 
 
 
