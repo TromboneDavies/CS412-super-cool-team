a9d0b1a7a62135f2f31f9bf1be62562c60ebd002
Luke Ogburn <21106956+logburn@users.noreply.github.com>
2022-04-27 14:44:06 -0400

    update

--- gen_test.py
+++ gen_test.py
@@ -4,13 +4,11 @@ import random
 def main():
     letters = "A B C D E F G H I J K L M N O P Q R S T U V W X Y".split()
     tests = {}
-    count = 0
     for line in itertools.product(list(letters), repeat=2):
         a, b = line
         if a != b and (b, a) not in tests:
-            count += 1
             tests[line] = random.randint(1, 10)
-    final = f"{count}\n"
+    final = f"{len(letters)}\n"
     for test in tests:
         a, b = test
         c = tests[test]
