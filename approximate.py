d87d470a617e6fd5dbe4b48ab46f7c5043b157dd
Luke Ogburn <21106956+logburn@users.noreply.github.com>
2022-05-01 13:51:18 -0400

    reogranzed

--- approximate.py
+++ approximate.py
@@ -46,58 +46,6 @@ def ACO():
         if n1 != n2:
             pheromones[edge] = pheromone_initial
     
-    # simulates an ant looking at its options and choosing a path
-    # randomly, but with some bias towards pheromone-heavy paths
-    def choose_node(ant_id):
-        # scores a node based on its pheromone level
-        def score(node):
-            score = (max_weight - weight[position[ant_id], node])
-            score += pheromones[position[ant_id], node]
-            return int(score) if score > 1 else 1
-        # return True if a node hasn't been visited by this ant
-        def valid(node):
-            return node not in marked[ant_id] and node != position[ant_id]
-        
-        bag = [n for n in nodes if valid(n)]
-        bag_weight = [score(n) for n in bag]
-        return random.choices(bag, bag_weight)[0]
-    
-    # increases pheromones between two given nodes of `trail`
-    def increase_pheromones(trail):
-        n1, n2 = trail
-        pheromones[n1, n2] += pheromone_increase
-    
-    # simulates time by dissapating the pheromone level of an edge
-    # without having to use a for loop to wait
-    def dissapate_pheromones(trail):
-        # dissapate the pheromones
-        pheromones[trail] += (pheromone_increase - (pheromone_decrease * weight[trail]))
-        # ensure that the minimum is there
-        if pheromones[trail] < pheromone_initial: pheromones[trail] = pheromone_initial
-    
-    # takes care of pheromone things as an ant travels on a given
-    # trail (aka path or edge)
-    def travel(trail):
-        increase_pheromones(trail)
-        dissapate_pheromones(trail)
-    
-    # run an ant. it will choose it's next node and go there, dropping
-    # pheromones as it goes and they will dissapate as he travels
-    def run_ant(ant_id):
-        # ensure we have nodes to visit
-        if len(nodes) == len(marked[ant_id]):
-            # go home if not there already
-            if position[ant_id] != start:
-                increase_pheromones((position[ant_id], start))
-                position[ant_id] = start
-            marked_all[ant_id] = True
-            return
-        # choose and visit next node
-        to_visit = choose_node(ant_id)
-        marked[ant_id].add(to_visit)
-        travel((position[ant_id], to_visit))
-        position[ant_id] = to_visit
-    
     # greedily chooses a path based on pheromone levels, NOT edge
     # weights. essentially chooses most traveled tour. returns a
     # tuple of the path and the cost, respectively
@@ -121,6 +69,58 @@ def ACO():
         cost += weight[path[-1], start]
         path.append(start)
         return path, cost
+    
+    # increases pheromones between two given nodes of `trail`
+    def increase_pheromones(trail):
+        n1, n2 = trail
+        pheromones[n1, n2] += pheromone_increase
+    
+    # simulates time by dissapating the pheromone level of an edge
+    # without having to use a for loop to wait
+    def dissapate_pheromones(trail):
+        # dissapate the pheromones
+        pheromones[trail] += (pheromone_increase - (pheromone_decrease * weight[trail]))
+        # ensure that the minimum is there
+        if pheromones[trail] < pheromone_initial: pheromones[trail] = pheromone_initial
+    
+    # takes care of pheromone things as an ant travels on a given
+    # trail (aka path or edge)
+    def travel(trail):
+        increase_pheromones(trail)
+        dissapate_pheromones(trail)
+    
+    # simulates an ant looking at its options and choosing a path
+    # randomly, but with some bias towards pheromone-heavy paths
+    def choose_node(ant_id):
+        # scores a node based on its pheromone level
+        def score(node):
+            score = (max_weight - weight[position[ant_id], node])
+            score += pheromones[position[ant_id], node]
+            return int(score) if score > 1 else 1
+        # return True if a node hasn't been visited by this ant
+        def valid(node):
+            return node not in marked[ant_id] and node != position[ant_id]
+        
+        bag = [n for n in nodes if valid(n)]
+        bag_weight = [score(n) for n in bag]
+        return random.choices(bag, bag_weight)[0]
+    
+    # run an ant. it will choose it's next node and go there, dropping
+    # pheromones as it goes and they will dissapate as he travels
+    def run_ant(ant_id):
+        # ensure we have nodes to visit
+        if len(nodes) == len(marked[ant_id]):
+            # go home if not there already
+            if position[ant_id] != start:
+                increase_pheromones((position[ant_id], start))
+                position[ant_id] = start
+            marked_all[ant_id] = True
+            return
+        # choose and visit next node
+        to_visit = choose_node(ant_id)
+        marked[ant_id].add(to_visit)
+        travel((position[ant_id], to_visit))
+        position[ant_id] = to_visit
     
     # run ants through the graph, timing their efforts
     stopwatch = time.time()
