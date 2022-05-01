8c408f29c54b0bd77a52b70bf8c24a6e2b9bf5ab
d3v3ns <sarmada@dukes.jmu.edu>
2022-05-01 15:21:27 -0400

    gentests

--- gen_test.py
+++ gen_test.py
@@ -3,19 +3,18 @@ import random
 
 def main():
     letters = "A B C D E F G H I J K L M N O P Q R S T U V W X Y".split()
-    letters2 = "A B C D E F G H I J K L M N".split()
     tests = {}
-    for line in itertools.product(list(letters2), repeat=2):
+    for line in itertools.product(list(letters), repeat=2):
         a, b = line
         if a != b and (b, a) not in tests:
             tests[line] = random.randint(1, 10)
-    final = f"{len(letters2)}\n"
+    final = f"{len(letters)}\n"
     for test in tests:
         a, b = test
         c = tests[test]
         final += f"{a} {b} {c}\n"
     
-    with open("tests/14nodes.txt", "w+") as f:
+    with open("tests/tests.txt", "w+") as f:
         f.write(final)
 
 if __name__ == "__main__":
