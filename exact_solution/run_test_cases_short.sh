5a858104a6a117bbd7a3ad1dfcfdaf350a87db56
d3v3ns <sarmada@dukes.jmu.edu>
2022-05-01 18:22:33 -0400

    all script files created

--- exact_solution/run_test_cases_short.sh
+++ exact_solution/run_test_cases_short.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# tests that run under 1 minute
 /usr/bin/python3 exact.py < test_cases/all_equal_weights.txt
 /usr/bin/python3 exact.py < test_cases/several_equal_weights.txt
 /usr/bin/python3 exact.py < test_cases/5nodes.txt
